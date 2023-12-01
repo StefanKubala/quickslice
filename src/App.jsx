cc975eaa38dbe96011a02fb41835187fc2f8a5bf
Stefan Kubala <stevokubala4445@gmail.com>
2023-12-01 15:18:07 +0100

    add basic file structure and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
---
+++ src/features/order/Order.jsx
@@ -0,0 +1,71 @@
+const order = {
+  id: "ABCDEF",
+  customer: "Jonas",
+  phone: "123456789",
+  address: "Arroios, Lisbon , Portugal",
+  priority: true,
+  estimatedDelivery: "2027-04-25T10:00:00",
+  cart: [
+    {
+      pizzaId: 7,
+      name: "Napoli",
+      quantity: 3,
+      unitPrice: 16,
+      totalPrice: 48,
+    },
+    {
+      pizzaId: 5,
+      name: "Diavola",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+    {
+      pizzaId: 3,
+      name: "Romana",
+      quantity: 1,
+      unitPrice: 15,
+      totalPrice: 15,
+    },
+  ],
+  position: "-9.000,38.000",
+  orderPrice: 95,
+  priorityPrice: 19,
+};
+
+function Order() {
+  const {
+    id,
+    status,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+    cart,
+  } = order;
+
+  return (
+    <div>
+      <div>
+        <h2>Status</h2>
+
+        <div>
+          {priority && <span>Priority</span>}
+          <span>{status} order</span>
+        </div>
+      </div>
+
+      <div>
+        <p>Estimated delivery:</p>
+      </div>
+
+      <div>
+        <p>Price pizza: </p>
+
+        <p>To pay on delivery:</p>
+      </div>
+    </div>
+  );
+}
+
+export default Order;
