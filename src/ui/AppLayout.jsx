00020b357ce6264e817dd7ab9b44da5fde4b21ec
Stefan Kubala <stevokubala4445@gmail.com>
2023-12-02 10:27:36 +0100

    add loader and fetching menu items from api

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  console.log(isLoading);
   return (
-    <>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
       </main>
       <CartOverview />
-    </>
+    </div>
   );
 }
