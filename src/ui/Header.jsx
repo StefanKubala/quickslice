545b29efd3c1a043d7e45e016f2dda05daefbd2a
Stefan Kubala <stevokubala4445@gmail.com>
2023-12-06 20:06:03 +0100

    add menu styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import SearchOrder from "../features/order/SearchOrder";
 export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-gradient-to-r from-yellow-400 to-red-600 ... px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-[5px]">
+      <Link to="/" className="tracking-[5px] italic">
         🍕Quickslice
       </Link>
 
