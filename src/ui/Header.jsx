a03f6fb518259f68946f600ed8f102beff8f8a23
Stefan Kubala <stevokubala4445@gmail.com>
2023-12-06 19:55:12 +0100

    add tailwind and styling to header and cartOverview

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,8 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Quickslice 🍕</Link>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-gradient-to-r from-yellow-400 to-red-600 ... px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-[5px]">
+        🍕Quickslice
+      </Link>
 
       <SearchOrder />
     </header>
